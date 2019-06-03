4dfd3605f1b8e0704556c6cfe6807dab032c113d
rahul mittapalli <mittapalli.rahul@gmail.com>
2019-06-03 10:43:41 +0530

    Update login.java

--- src/Add/login.java
+++ src/Add/login.java
@@ -8,8 +8,8 @@ public class login {
 		// WebDriver driver=new SafariDriver();
 		driver.get("https://dev-pim.dermalogica.com");	
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@type='text']")).sendKeys("mittapalli.rahul@gmail.com");
-		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("12345678");
+		driver.findElement(By.xpath("//*[@type='text']")).sendKeys("myemail");
+		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("password1to8");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		Thread.sleep(5000);
 	}
